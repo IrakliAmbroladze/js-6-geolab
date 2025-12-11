8a1bdf15c16f20c042be3a597780b1a9e3ead01e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-11 06:47:45 +0400

    feat: create product add function

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const cart = [
 ];
 
 const sumCart = () => cart.reduce((acc, cur) => acc + cur.price, 0);
+const addProduct = (product) => [...cart, product];
 
 console.log("cart sum is: ", sumCart());
+console.log(
+  "new cart is: ",
+  addProduct({ name: "Monitor", price: 250, quantity: 3 }),
+);
